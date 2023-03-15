1e79d6249f69d73fdd394b8405bb5ea4102a64ab
SagarPrasad160 <sagarprasad916@gmail.com>
2023-03-15 20:37:46 +0530

    add state

--- src/components/Expenses/ExpenseTracker.js
+++ src/components/Expenses/ExpenseTracker.js
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
 function Expense(props) {
+  const [amount, setAmount] = useState(props.amount);
+  function handleChange() {
+    const newAmount = prompt("enter new amount");
+    setAmount(newAmount);
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
         <div>{props.location}</div>
-        <button
-          onClick={(event) => {
-            const expenseItem = event.target.parentNode.parentNode;
-            expenseItem.remove();
-          }}
-        >
-          Delete
-        </button>
+        <button onClick={handleChange}>Update!</button>
       </div>
     </Card>
   );
