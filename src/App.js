e2591da40b94667b1c67ab74ee81a60824bda721
SagarPrasad160 <sagarprasad916@gmail.com>
2023-03-15 12:21:50 +0530

    add delete button

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ function App() {
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
+      location: "NYC",
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "DN",
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
+      location: "TN",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
+      location: "LA",
     },
   ];
 
--- src/components/Expenses/ExpenseTracker.js
+++ src/components/Expenses/ExpenseTracker.js
@@ -12,6 +12,14 @@ function Expense(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <div>{props.location}</div>
+        <button
+          onClick={(event) => {
+            const expenseItem = event.target.parentNode.parentNode;
+            expenseItem.remove();
+          }}
+        >
+          Delete
+        </button>
       </div>
     </Card>
   );
